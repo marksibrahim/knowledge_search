9d85550e0bd9d69a1225217e41f0cb2f51c74caa
marksibrahim <mark.ibhm@gmail.com>
2016-06-13 12:32:54 -0400

    working read from json to graph db

--- neo4j_play/test_neo.py
+++ neo4j_play/test_neo.py
@@ -1,20 +1,21 @@
-from py2neo import Graph
-from py2neo import Node, Relationship
+from py2neo import Graph, Node, Relationship
 
-# set up authentication parameters
-# authenticate("localhost:7474", "neo4j", "neo4j")
-
-# a good guide:
-    # http://py2neo.org/2.0/intro.html
-
-graph_db = Graph()
-# default connection: 
-    # http://localhost:7474/db/data/
-
-a = Node("article", title="A", views=2)
-b = Node("article", title="B", views=3)
-ab = Relationship(a, "PARENT", b)
-
-graph_db.create(ab)
+g = Graph()
+tx = g.begin()
+a = Node("Person", name="Alice")
+tx.create(a)
+b = Node("Person", name="Bob")
+ab = Relationship(a, "KNOWS", b)
+tx.create(ab)
+tx.commit()
+g.exists(ab)
 
 
+"""
+Sample Query
+>>> from py2neo import Graph
+>>> g = Graph()
+>>> g.run("MATCH (a) WHERE a.name={x} RETURN a.name", x="Bob").evaluate()
+u'Bob'
+>>>
+"""
---
+++ neo4j_play/write_json_to_neo.py
@@ -0,0 +1,40 @@
+from py2neo import Graph, Node, Relationship
+
+import json
+
+results_path = "/Users/mark/Desktop/wiki_v4/"
+with open(results_path + "sample_fln.json") as f:
+        fln_dict = json.load(f)
+
+# initialize graph db
+g = Graph()
+tx = g.begin()
+
+
+# write to graph db
+
+for article, fl in fln_dict.iteritems():
+    article_node = Node("Article", title=article)
+    fl_node = Node("Article", title=fl)
+    relation = Relationship(article_node, "LINKS_TO", fl_node)
+
+    tx.create(article_node)
+    tx.create(fl_node)
+    tx.create(relation)
+
+tx.commit()
+
+
+"""
+g = Graph()
+tx = g.begin()
+
+a = Node("Person", name="Alice")
+tx.create(a)
+b = Node("Person", name="Bob")
+ab = Relationship(a, "KNOWS", b)
+tx.create(ab)
+
+tx.commit()
+g.exists(ab)
+"""
