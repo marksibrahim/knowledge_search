a5757b3dd09fc15a7c17533c17463286cc3eb995
marksibrahim <mark.ibhm@gmail.com>
2016-06-21 11:56:40 -0400

    working optimized queries

---
+++ Knowledge_search/get_daily_page_views.py
@@ -0,0 +1,81 @@
+"""
+combines hourly page views into a single csv on S3
+"""
+
+import pandas as pd
+import urllib
+import datetime
+import csv
+import os
+
+def build_df(year, month, day, hour):
+    """
+    returns a pandas dataframe with columns:
+        project, article, views, content_returned
+    based on input date string:
+        e.g, "2016", "06", "24", "01"
+    files are found at
+            https://dumps.wikimedia.org/other/pagecounts-all-sites/2016/2016-05/pagecounts-20160501-000000.gz
+    """
+    base_url_string = "https://dumps.wikimedia.org/other/pagecounts-all-sites/"
+    url_string = base_url_string + "/" + year + "/" + year + "-" + month
+    url_string += "/pagecounts-" + year + month + day + "-" + hour + "0000.gz"
+    
+    file_name = year + month + day + "-" + hour + ".gz"
+    
+    # download file
+    opener = urllib.URLopener()    
+    opener.retrieve(url_string, file_name)
+    
+    # return data frame
+    df = pd.read_csv(file_name, compression="gzip", sep=" ", quoting=csv.QUOTE_NONE,
+                 error_bad_lines=False, header=None)
+    df.columns = ['project', 'article', 'views', 'content_returned']
+    # include only en-wiki
+    df = df[df['project'] == 'en']
+    df = df[['article', 'views']]
+
+    # remove gz file
+    os.remove(file_name)
+    
+    return df
+
+def combine_dataframes(df1, df2):
+    """
+    sums the page views in the given dataframes
+        uses an outer join (treating nan as 0)
+    returns a new dataframe
+        'article', 'views'
+    """
+    df_combined = df1.merge(df2, left_on='article', right_on='article', how='outer')
+    df_combined.fillna(0, inplace=True)
+    df_combined['views'] = df_combined['views_x'] + df_combined['views_y']
+    df_combined = df_combined[['article', 'views']]
+    df_combined = df_combined.drop_duplicates('article')
+    
+    return df_combined
+
+if __name__ == "__main__":
+    # get today's date
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = str(now.month).zfill(2)
+    day = str(now.day - 1).zfill(2)
+
+    # write first data frame to a file
+    df1 =  build_df(year, month, day, "00")
+    df1.to_csv(year + month + day + "_views.csv")
+
+    hours = [str(h).zfill(2) for h in range(24)]
+
+    for hour1, hour2 in zip(hours[1::2], hours[2::2]):
+        print hour2
+        df1 = build_df(year, month, day, hour1)
+        df2 = build_df(year, month, day, hour2)
+        df1_df2 = combine_dataframes(df1, df2)
+
+        # combine with existing views 
+        df_existing = pd.read_csv(year + month + day + "_views.csv")
+        df_combined = combine_dataframes(df1_df2, df_existing)
+
+        df_combined.to_csv(year + month + day + "_views.csv")
---
+++ Knowledge_search/get_neighbors.py
@@ -0,0 +1,57 @@
+from py2neo import Graph, authenticate
+
+temp_pass = "neo4j1"
+
+
+class Network():
+    """
+    connects and queries the first link network stored in Neo4j
+    """
+
+    def __init__(self, article):
+        """
+        authenticates connection to the graph database
+        and stores article neighbors as
+            parent_article (higher category)
+            comprable_articles
+            child_articles (details)
+        """
+        # connect to database
+        authenticate("52.204.244.120:7474", "neo4j", temp_pass)
+        self.g = Graph("http://52.204.244.120:7474/db/data/")
+
+        self.article = article
+        self.parent_article = self.get_parent_article() 
+        self.comprable_articles = self.get_comprable_articles()
+        self.child_articles = self.get_child_articles()
+
+    def get_parent_article(self):
+        """
+        returns article's the first link 
+        """
+        query = "MATCH (a:Article {title:{x}})-[r:FL_TO]->(n) "
+        query += "USING INDEX a:Article(title) RETURN n.title"
+        result = self.g.run(query, x=self.article).evaluate()
+        return result
+
+    def get_comprable_articles(self):
+        """
+        returns a list of articles linking to the same parent node
+        """
+        query = "MATCH (n)-[r:FL_TO]->(a: Article {title: {x}}) "
+        query += "USING INDEX a:Article(title) RETURN n.title "
+        query += "LIMIT 3"
+        result = self.g.run(query, x=self.parent_article).evaluate()
+        return result
+
+    def get_child_articles(self):
+        """
+        returns a list of child nodes 
+        (detailed topics linking to the article)
+        """
+        query = "MATCH (n)-[r:FL_TO]->(a: Article {title: {x}}) "
+        query += "USING INDEX a:Article(title) RETURN n.title "
+        query += "LIMIT 7"
+        result = self.g.run(query, x=self.article).evaluate()
+        return result
+        
