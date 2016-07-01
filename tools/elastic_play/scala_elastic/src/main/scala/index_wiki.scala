f0eaba9abb741090cf315ea8a49f46a604ba808d
marksibrahim <mark.ibhm@gmail.com>
2016-07-01 14:57:08 -0400

    test scala map on df

--- tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
+++ tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
@@ -5,14 +5,16 @@ import org.apache.spark.sql.SQLContext._
 import org.elasticsearch.spark.sql._
 import org.apache.spark._
 
-
-
 object WikiIndex {
     def main(args: Array[String]): Unit = {
     /* initialize a spark context */
-        val conf = new SparkConf().setAppName("Load cached model")
+        val conf = new SparkConf().setAppName("Oleg")
         val sc = new SparkContext(conf)
 
+    /* read xml into a dataframe */
         val sqlContext = new SQLContext(sc)
         val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("s3a://wiki-xml-dump/sample_dump.xml")
+        val transformed_df = df.select("title", "revision").map(article => "title: " + t(0)).first().foreach(println)
+
+
 }}
