bb2f2c0c9cf582fe8e23bef2b51ed5dbb142eddd
marksibrahim <mark.ibhm@gmail.com>
2016-07-01 14:31:47 -0400

    specify a spark context

--- tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
+++ tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
@@ -2,9 +2,17 @@ package my_wiki
 
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SQLContext._
+import org.elasticsearch.spark.sql._
+import org.apache.spark._
+
+
 
 object WikiIndex {
     def main(args: Array[String]): Unit = {
+    /* initialize a spark context */
+        val conf = new SparkConf().setAppName("Load cached model")
+        new SparkContext(conf)
+
         val sqlContext = new SQLContext(sc)
         val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("s3a://wiki-xml-dump/sample_dump.xml")
 }}
