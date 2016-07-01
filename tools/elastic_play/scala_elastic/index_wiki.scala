38d75a55e746dac856a698c77c873a705c861328
marksibrahim <mark.ibhm@gmail.com>
2016-07-01 14:01:53 -0400

    added package to scala file

--- tools/elastic_play/scala_elastic/index_wiki.scala
+++ tools/elastic_play/scala_elastic/index_wiki.scala
@@ -1,6 +1,9 @@
+package my_wiki
+
 import org.apache.spark.sql.SQLContext
 
-val sqlContext = new SQLContext(sc)
-val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("s3a://wiki-xml-dump/sample_dump.xml") 
-
-
+object WikiIndex {
+    def main(args: Array[String]): Unit = {
+        val sqlContext = new SQLContext(sc)
+        val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("s3a://wiki-xml-dump/sample_dump.xml")
+}}
