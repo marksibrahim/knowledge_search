21d232dd0677ef0f854bce8d37e1226c73151666
marksibrahim <mark.ibhm@gmail.com>
2016-07-01 18:38:56 -0400

    working scala spark-submit

--- tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
+++ tools/elastic_play/scala_elastic/src/main/scala/index_wiki.scala
@@ -14,7 +14,20 @@ object WikiIndex {
     /* read xml into a dataframe */
         val sqlContext = new SQLContext(sc)
         val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("s3a://wiki-xml-dump/sample_dump.xml")
-        val transformed_df = df.select("title", "revision").map(article => "title: " + t(0)).first().foreach(println)
+        val map_df = df.select("title", "revision").map(article => Map("title " -> article(0),  " body_text " -> article(1))).first()
+        val transformed_df = df.select("title", "revision").map(article => ("title: " + article(0) + " body_text: " + article(1))).map(t_a => t_a.slice(0, 2000))
+        val intermediary_df = transformed_df.first()
+        val punc_df = transformed_df.map(t_a => t_a.replaceAll("""[\p{Punct}&&[^:]]""", " ")).first()
+        println("########################################")
+        println("########################################")
+        println(map_df)
+        println("########################################")
+        println("########################################")
 
-
-}}
+        println("########################################")
+        println("########################################")
+        println(punc_df)
+        println("########################################")
+        println("########################################")
+}
+}
