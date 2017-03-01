04377e5605efcdf8d3d593d4e08a1def99b5039e
marksibrahim <mark.ibhm@gmail.com>
2017-02-28 22:54:48 -0500

    added sample response page

--- site/app.py
+++ site/app.py
@@ -1,6 +1,7 @@
 """
 Flask App
 """
+from __future__ import print_function
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -14,23 +15,33 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    title = request.form['title']
-    node = get_neighbors.Network(title)
-    parent = node.parent_article[0]
-    comprable = ", ".join([article[0] for article in node.comprable_articles])
-    #return node.child_articles[0][0]
-    children = ", ".join([article[0] for article in node.child_articles])
+    try:
+        title = request.form['title']
+        node = get_neighbors.Network(title)
+        parent = node.parent_article[0]
+        comprable = ", ".join([article[0] for article in node.comprable_articles])
+        #return node.child_articles[0][0]
+        children = ", ".join([article[0] for article in node.child_articles])
 
-    #data
-    node_views = node.build_json_nodes_views()
-    connections = node.build_json_node_connections()
-    return render_template("d3_response.html", article=node.article, parent=parent,
-            comprable=comprable, children=children, node_views=node_views, 
-            real_connections=connections)
+        #data
+        node_views = node.build_json_nodes_views()
+        connections = node.build_json_node_connections()
+        return render_template("d3_response.html", article=node.article, parent=parent,
+                comprable=comprable, children=children, node_views=node_views, 
+                real_connections=connections)
+    except Exception:
+        print("Call to DB Failed")
+        print("==================")
+        print(Exception)
+        return render_template("sample_response.html")
 
 @app.route('/demo')
 def my_demo():
     return render_template("demo.html")
+
+@app.route('/sample_response')
+def sample_response():
+    return render_template("sample_response.html")
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000)
